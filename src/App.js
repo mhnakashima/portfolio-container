16453e75b8272645be4bff2f93c2068eda91eb3b
Michael Nakashima <mhnakashima@gmail.com>
2024-05-20 14:22:07 -0300

    module federalization on Main Container

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import InfoApp from './components/InfoApp';
+
+
+const App = () => {
+
+    return (
+        <>
+            {/* Microfront with Main Info */}
+            <aside id="app-info">
+                <InfoApp />
+            </aside>
+            <main className="menu">
+                {/* Micro with status, menu and Language Selection*/}
+                <div className="app-menu">
+
+                </div>
+
+                {/* Navigation Sections using microfront*/}
+
+                {/* App Experience */}
+                <section id="app-experience" className="app">
+
+                </section>
+
+                {/* App Portfolio */}
+                <section id="app-portfolio" className="app">
+
+                </section>
+
+                {/* App Contact me */}
+                <section id="app-contact" className="app">
+
+                </section>
+
+                {/* App FAQ */}
+                <section id="app-faq" className="FAQ">
+
+                </section>
+            </main>
+        </>
+    )
+}
+
+export default App;
